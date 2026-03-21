92428de4aace6bd952bbb81844384b129fd5b7fd
Burzah <116982774+Burzah@users.noreply.github.com>
2026-03-21 00:45:26 -0700

    optimization pass

--- ColorMyStamina/scripts/mods/ColorMyStamina/ColorMyStamina.lua
+++ ColorMyStamina/scripts/mods/ColorMyStamina/ColorMyStamina.lua
@@ -18,9 +18,38 @@ local COLORS = {
     black   = {255,   0,   0,   0},
 }
 
+local cached_thresholds = {}
+local cached_default_color = "green"
+
+local function rebuild_cache()
+    local t1 = mod:get("threshold_1") or 25
+    local t2 = mod:get("threshold_2") or 50
+    local t3 = mod:get("threshold_3") or 75
+    local t4 = mod:get("threshold_4") or 100
+    local c1 = mod:get("color_1") or "red"
+    local c2 = mod:get("color_2") or "orange"
+    local c3 = mod:get("color_3") or "yellow"
+    local c4 = mod:get("color_4") or "green"
+
+    cached_thresholds = {
+        {value = t1, color = c1},
+        {value = t2, color = c2},
+        {value = t3, color = c3},
+        {value = t4, color = c4},
+    }
+    table.sort(cached_thresholds, function(a, b) return a.value < b.value end)
+    cached_default_color = cached_thresholds[#cached_thresholds].color
+end
+
+rebuild_cache()
+
+mod.on_setting_changed = function()
+    rebuild_cache()
+end
+
 mod:hook("HudElementStamina", "_draw_stamina_chunks", function(func, self, dt, t, ui_renderer)
-if not mod:is_enabled() then
-    return func(self, dt, t, ui_renderer)
+    if not mod:is_enabled() then
+        return func(self, dt, t, ui_renderer)
     end
 
     local stamina_fraction = self._stamina_fraction or 0
@@ -32,54 +61,37 @@ if not mod:is_enabled() then
         if player_unit_data then
             local stamina_comp = player_unit_data:read_component("stamina")
             stamina_fraction = stamina_comp and stamina_comp.current_fraction or stamina_fraction
-            end
-            end
+        end
+    end
 
-            local t1 = mod:get("threshold_1") or 25
-            local t2 = mod:get("threshold_2") or 50
-            local t3 = mod:get("threshold_3") or 75
-            local t4 = mod:get("threshold_4") or 100
-            local c1 = mod:get("color_1") or "red"
-            local c2 = mod:get("color_2") or "orange"
-            local c3 = mod:get("color_3") or "yellow"
-            local c4 = mod:get("color_4") or "green"
+    local percent = stamina_fraction * 100
+    local color_key = cached_default_color
 
-            local percent = stamina_fraction * 100
+    for i = 1, #cached_thresholds do
+        if percent <= cached_thresholds[i].value then
+            color_key = cached_thresholds[i].color
+            break
+        end
+    end
 
-            local thresholds = {
-                {value = t1, color = c1},
-                {value = t2, color = c2},
-                {value = t3, color = c3},
-                {value = t4, color = c4},
-            }
-            table.sort(thresholds, function(a, b) return a.value < b.value end)
+    local rgba = COLORS[color_key] or COLORS["green"]
 
-            local color_key = thresholds[#thresholds].color
-            for i = 1, #thresholds do
-                if percent <= thresholds[i].value then
-                    color_key = thresholds[i].color
-                    break
-                    end
-                    end
+    local widget = self._widgets_by_name.stamina_bar
+    if widget then
+        if widget.style.bar_fill then
+            local c = widget.style.bar_fill.color
+            c[1], c[2], c[3], c[4] = rgba[1], rgba[2], rgba[3], rgba[4]
+        end
+        if widget.style.bar_spent then
+            local sc = widget.style.bar_spent.color
+            sc[1], sc[2], sc[3], sc[4] = 150, rgba[2], rgba[3], rgba[4]
+        end
+    end
 
-                    local rgba = COLORS[color_key] or COLORS["green"]
+    if self._stamina_nodge_widget then
+        local nc = self._stamina_nodge_widget.style.nodges.color
+        nc[1], nc[2], nc[3], nc[4] = rgba[1], rgba[2], rgba[3], rgba[4]
+    end
 
-                    local widget = self._widgets_by_name.stamina_bar
-                    if widget then
-                        if widget.style.bar_fill then
-                            local c = widget.style.bar_fill.color
-                            c[1], c[2], c[3], c[4] = rgba[1], rgba[2], rgba[3], rgba[4]
-                            end
-                            if widget.style.bar_spent then
-                                local sc = widget.style.bar_spent.color
-                                sc[1], sc[2], sc[3], sc[4] = 150, rgba[2], rgba[3], rgba[4]
-                                end
-                                end
-
-                                if self._stamina_nodge_widget then
-                                    local nc = self._stamina_nodge_widget.style.nodges.color
-                                    nc[1], nc[2], nc[3], nc[4] = rgba[1], rgba[2], rgba[3], rgba[4]
-                                    end
-
-                                    return func(self, dt, t, ui_renderer)
-                                    end)
+    return func(self, dt, t, ui_renderer)
+end)
